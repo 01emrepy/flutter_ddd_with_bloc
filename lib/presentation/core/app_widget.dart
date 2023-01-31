e32ac46955ace0cfc8b7d038285a0e0ac21e6f3f
Emre Yıldız <ey56748@gmail.com>
2023-02-01 00:03:12 +0300

    firebase

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ddd_with_cubit/presentation/pages/home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
