e38bb34535d58f07a36f8ec15c42c08b784982f4
Emre Yıldız <ey56748@gmail.com>
2023-02-01 16:51:12 +0300

    injection config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ddd_with_cubit/firebase_options.dart';
+import 'package:flutter_ddd_with_cubit/injection.dart';
 import 'package:flutter_ddd_with_cubit/presentation/core/app_widget.dart';
 
 void main() async {
@@ -8,5 +9,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  configureDependencies();
   runApp(const AppWidget());
 }
