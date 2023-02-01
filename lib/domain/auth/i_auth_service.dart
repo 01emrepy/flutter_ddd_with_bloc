bb9ef682ce1902465a7bfd13824885f1642d99ac
Emre Yıldız <ey56748@gmail.com>
2023-02-01 17:32:53 +0300

    firebase_service

---
+++ lib/domain/auth/i_auth_service.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_ddd_with_cubit/domain/auth/auth_user_model.dart';
+
+abstract class IAuthService {
+  Stream<AuthUserModel> get authStateChanges;
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signOut();
+
+  Future<void> signInWithGoogle();
+}
